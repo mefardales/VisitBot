861200cf506074db83239bd8a7b35a331aa01e1a
manuel <mesfardales@protonmail.com>
2021-09-30 14:07:46 -0500

    Url handles

---
+++ info.py
@@ -0,0 +1,2 @@
+info = "🤓Genera vistas de forma automática en sitios de internet \n\n" \
+       "❌Agrega una dirección de internet para generar views❌"
--- main.py
+++ main.py
@@ -1,32 +1,64 @@
-from views.views_generate import urls_views, close_connection
-import telebot
 import requests
 import re
+import telebot
+from views.views_generate import urls_views, close_connection
+from info import info
 
+# CONSTANTS
+INFO = info
+N_VIEWS = 0
 bot = telebot.TeleBot("2033192880:AAEBcU8OtU0AkVATJp_ak9LvGsWZCrh6zF0")
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "🤓Genera vistas de forma automática en sitios de internet \n\n"
-                          "❌Agrega una dirección de internet para generar views❌")
+    bot.reply_to(message, INFO)
 
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-#     print(message.text)
+# Handling a list of web sites
+@bot.message_handler(commands=['list'])
+def handle_list(message):
+    cleaned_ = message.text.replace('/list', '')
+    url_list = list(cleaned_.split(","))
+    print(url_list)
+
+
+# Setting views to visit web pages
+@bot.message_handler(commands=['nviews'])
+def set_views(message):
+    int_message = int(message.text.replace('/nviews', ''))
+    if int_message != 0:
+        if int_message <= 1000:
+            global N_VIEWS
+            N_VIEWS = int_message
+            bot.send_message(message.chat.id, f"{int_message}")
+            print(f"cantidad de views --> {int_message}")
+        else:
+            bot.send_message(message.chat.id, "cantidad de views diarios no pueden ser mayor que --> 1000")
+            print("cantidad de views diarios no pueden ser mayor que --> 1000")
+    else:
+        bot.send_message(message.chat.id, "-")
+
+
+# Handler url and visit
 @bot.message_handler(func=lambda message: True)
 def view_web(message):
     # Check url
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s(" \
-            r")<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])) "
-    url = re.match(regex, message.text)
-    if url:
-        # for item in list(message.text):
-        urls_views(message.text, delay=5)
-        bot.send_message(message.chat.id, "Finalizado")
-        close_connection()
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    if re.match(regex, message.text) is not None:
+        try:
+            urls_views(message.text, delay=5)
+            bot.send_message(message.chat.id, "Conexión exitosa")
+            # close_connection()
+        except Exception as ex:
+            bot.send_message(message.chat.id, f"Se produjo un error al conectarse a la página {ex}")
+
     else:
         bot.send_message(message.chat.id, " Error !!! \n Inserte direcciones de internet correctas !!!")
 
